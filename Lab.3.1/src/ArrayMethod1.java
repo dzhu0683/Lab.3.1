e960f2689b9c67b1271f9485504103634659eddc
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2018-01-18 09:12:48 -0500

    e

---
+++ Lab.3.1/src/ArrayMethod1.java
@@ -0,0 +1,40 @@
+
+
+public class ArrayMethod1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int binarysearch(int [] list1, int query)
+	{
+		int L = 0;
+		int R = list1.length - 1;
+		while(L <= R)
+		{
+			int mid = L + (R-L)/2;
+			if(list1[mid] > query)
+			{
+				
+			}
+		}
+	}
+	
+	public static void insertionSort(int[] list1)
+	{
+		
+	}
+	
+	public static void selectionSort(double[] list1)
+	{
+		
+	}
+	
+	public static void bubbleSort(String[] list1)
+	{
+		for(x = list.length, )
+	}
+
+}
