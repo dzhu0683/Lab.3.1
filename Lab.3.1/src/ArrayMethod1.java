69fa1a71d2537e9d16a8c560cf886de5d2889dd2
Darren <Darren@Darren-PC>
2018-01-30 22:38:27 -0500

    f

--- Lab.3.1/src/ArrayMethod1.java
+++ Lab.3.1/src/ArrayMethod1.java
@@ -15,7 +15,7 @@ public class ArrayMethod1
 		insertionSort(test1);
 		long end = System.nanoTime();
 		long time = end - start;
-		System.out.println("Test1 took: " + time + "nanoseconds");
+		System.out.println("Test1 took: " + time + " nanoseconds");
 		System.out.println(Arrays.toString(test1));
 		
 		//Selection Sort Test
@@ -23,7 +23,7 @@ public class ArrayMethod1
 		selectionSort(test2);
 		end = System.nanoTime();
 		time = end - start;
-		System.out.println("Test2 took: " + time + "nanoseconds");
+		System.out.println("Test2 took: " + time + " nanoseconds");
 		System.out.println(Arrays.toString(test2));
 		
 		//Bubble Sort Test
@@ -31,30 +31,43 @@ public class ArrayMethod1
 		bubbleSort(test3);
 		end = System.nanoTime();
 		time = end - start;
-		System.out.println("Test3 took: " + time + "nanoseconds");
+		System.out.println("Test3 took: " + time + " nanoseconds");
 		System.out.println(Arrays.toString(test3));
 
 	}
 	
 	public static void insertionSort(int[] list1)
 	{
-		for(int i = 1; i < list1.length; i++)
+		for(int i = 1; i < list1.length; i++)// for loop that starts at second num in array
 		{
-			int k = list1[i];
 			int j = i - 1;
 			
-			while(j >= 0 && list1[j] > k)
+			while(j >= 0 && list1[j] > list1[i])//as long as the value at index j in greater than value at index i
 			{
-				list1[j+1] = list1[j];
-				j = j - 1;
+				list1[j+1] = list1[j];//replace value after index j with value at index j
+				j = j - 1;//j - 1 so you can continue to check all values in the front of i until j reachs 0
 			}
-			list1[j+1] = k;
+			list1[j+1] = list1[i]; //replace value after index j with value at index i
 		}
 	}
 	
 	public static void selectionSort(double[] list1)
 	{
-		
+		for(int i = 0; i < list1.length -1; i++)// for loop to pick out the first number to compare
+		{
+			int min = i;//set the 1st value as min
+			for(int j = i+1; j < list1.length; j++)//for loop to pick out the num that comes after
+			{
+				if(list1[j] < list1[min])//if the second number is less than first
+				{
+					min = j;//if it is set that second number as the new min
+				}
+			}
+			//now we swap the two values
+			double temp = list1[min];//create temporay variable to store min
+			list1[min] = list1[i];//replace the second value with the first value
+			list1[i] = temp;//place the min in the first index
+		}
 	}
 	
 	public static void bubbleSort(String[] list1)
