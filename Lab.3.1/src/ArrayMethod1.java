48067b030ca5ee7ada093072c8737746804d3471
Darren <Darren@Darren-PC>
2018-01-30 22:10:17 -0500

    f

--- Lab.3.1/src/ArrayMethod1.java
+++ Lab.3.1/src/ArrayMethod1.java
@@ -1,38 +1,55 @@
+import java.util.Arrays;
+
 public class ArrayMethod1 
 {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		//Testing Array
+		int [] test1 = {1, 4, 4, 5, 2, 4, 3, 17, 0};
+		double [] test2 = {1.1, 4.1, 4.2, 5.0, 2.0, 4.0, 3.0, 17.0, 0.0};
+		String [] test3 = {"zebra", "tortilla", "abba", "foo", "bar", "aba"};
+		
+		//Insertion Sort Test
+		long start = System.nanoTime();
+		insertionSort(test1);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Test1 took: " + time + "nanoseconds");
+		System.out.println(Arrays.toString(test1));
+		
+		//Selection Sort Test
+		start = System.nanoTime();
+		selectionSort(test2);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Test2 took: " + time + "nanoseconds");
+		System.out.println(Arrays.toString(test2));
+		
+		//Bubble Sort Test
+		start = System.nanoTime();
+		bubbleSort(test3);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Test3 took: " + time + "nanoseconds");
+		System.out.println(Arrays.toString(test3));
 
-	}
-	
-	public static int binarysearch(int [] list1, int query)
-	{
-		int L = 0;
-		int R = list1.length - 1;
-		while(L <= R)
-		{
-			int mid = L + (R-L)/2;
-			if(list1[mid] > query)
-			{
-				L = mid;
-			}
-			if (list1[mid] < query)
-			{
-				R = mid;
-			}
-			else
-			{
-				return mid;
-			}
-		}
-		return -1;
 	}
 	
 	public static void insertionSort(int[] list1)
 	{
-		
+		for(int i = 1; i < list1.length; i++)
+		{
+			int k = list1[i];
+			int j = i - 1;
+			
+			while(j >= 0 && list1[j] > k)
+			{
+				list1[j+1] = list1[j];
+				j = j - 1;
+			}
+			list1[j+1] = k;
+		}
 	}
 	
 	public static void selectionSort(double[] list1)
@@ -42,10 +59,7 @@ public class ArrayMethod1
 	
 	public static void bubbleSort(String[] list1)
 	{
-		for(int x = list1.length; x >= 0; x++)
-		{
-			
-		}
+		
 	}
 
 }
